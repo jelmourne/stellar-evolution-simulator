a3b3fa2f87013e6c3c1daf49fe7471602d2f0bd9
Justin Elmourne <jelmourne@outlook.com>
2023-11-04 12:47:15 -0400

    Scene

--- visualizer/scripts/main.js
+++ visualizer/scripts/main.js
@@ -4,27 +4,31 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  7,
+  40
 );
 
-const geometry = new THREE.SphereGeometry();
+const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshBasicMaterial({ color: "#ff00ff" });
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-camera.position.z = 3;
+const light = new THREE.PointLight("#ffffff", 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
 
-const renderer = new THREE.WebGLRenderer();
+camera.position.z = 10;
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setClearColor(0x000000, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.01;
-
   renderer.render(scene, camera);
+  sphere.rotation.x += 0.01;
 }
 
 animate();
