2551a3929bd4221fef22a2c54e69ea2f43624e82
Justin Elmourne <jelmourne@outlook.com>
2023-11-04 10:50:05 -0400

    Sphere

--- visualizer/scripts/main.js
+++ visualizer/scripts/main.js
@@ -2,28 +2,27 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
+const geometry = new THREE.SphereGeometry();
+const material = new THREE.MeshBasicMaterial({ color: "#ff00ff" });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+camera.position.z = 3;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry();
-const material = new THREE.MeshBasicMaterial({ color: "#ff00ff" });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  sphere.rotation.x += 0.01;
 
   renderer.render(scene, camera);
 }
