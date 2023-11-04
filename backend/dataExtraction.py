2be9ae73ae583890d4f650f2965690707ab557b7
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2023-11-04 14:43:26 -0400

    Adding another renderer to display labels and legend

--- backend/dataExtraction.py
+++ backend/dataExtraction.py
@@ -7,6 +7,7 @@ filePath = r"C:\Users\Tristan\Desktop\trimmed_history.data"
 # Using pandas to extract the data into a DataFrame for data manipulation
 result = pd.DataFrame(mesa.read_history(filePath))
 
-starData = result[['model_number', 'star_age', 'star_mass', 'log_L', 'log_R', 'log_Teff', 'log_center_Rho', 'log_center_P', 'center_h1', 'center_he3', 'center_he4', 'star_mdot']].copy()
-starData.to_csv('data.csv', index=False)
+print(result)
+#starData = result[['model_number', 'star_age', 'star_mass', 'log_L', 'log_R', 'log_Teff', 'log_center_Rho', 'log_center_P', 'center_h1', 'center_he3', 'center_he4', 'star_mdot']].copy()
+# starData.to_csv('data.csv', index=False)
 
--- visualizer/scripts/main.js
+++ visualizer/scripts/main.js
@@ -1,4 +1,6 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -9,11 +11,11 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshBasicMaterial({ color: "#ff00ff" });
+const material = new THREE.MeshBasicMaterial({ color: '#ff00ff' });
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-const light = new THREE.PointLight("#ffffff", 1, 100);
+const light = new THREE.PointLight('#ffffff', 1, 100);
 light.position.set(0, 10, 10);
 scene.add(light);
 
@@ -24,11 +26,26 @@ renderer.setClearColor(0x000000, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Creating renderer to place labels and the legend
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+labelRenderer.domElement.style.pointerEvents = 'none'; // Ignoring mouse events so orbit controls still work
+document.body.appendChild(labelRenderer.domElement);
+
 function animate() {
   requestAnimationFrame(animate);
-
   renderer.render(scene, camera);
+  labelRenderer.render(scene, camera);
   sphere.rotation.x += 0.01;
 }
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  labelRenderer.setSize(this.window.innerWidth, this.window.innerHeight);
+});
 
 animate();
