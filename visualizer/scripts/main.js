40f44c63fd639a64846340fe496e981003469b55
Justin Elmourne <jelmourne@outlook.com>
2023-11-04 14:23:59 -0400

    Sphere

--- visualizer/scripts/main.js
+++ visualizer/scripts/main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { getSunTexture } from "./helpers";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -8,8 +10,13 @@ const camera = new THREE.PerspectiveCamera(
   40
 );
 
+var bgTexture = new THREE.TextureLoader().load("/2k_stars.jpg");
+bgTexture.minFilter = THREE.LinearFilter;
+scene.setClearColor = new THREE.Color(0, 0, 0);
+scene.background = bgTexture;
+
 const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshBasicMaterial({ color: "#ff00ff" });
+const material = getSunTexture();
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
@@ -22,13 +29,17 @@ camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setClearColor(0x000000, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
-  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.002;
 }
 
 animate();
+
+// Orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
