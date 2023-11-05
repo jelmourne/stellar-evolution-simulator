aa22abcd8387f294a333c1d7f22d088b262a997f
Justin Elmourne <jelmourne@outlook.com>
2023-11-05 10:34:06 -0500

    Final project

--- visualizer/scripts/main.js
+++ visualizer/scripts/main.js
@@ -1,9 +1,5 @@
 import * as THREE from "three";
 import "../style.css";
-import {
-  CSS2DRenderer,
-  CSS2DObject,
-} from "three/examples/jsm/renderers/CSS2DRenderer";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
@@ -212,4 +208,3 @@ for (let i = 0; i < length; i += 100) {
   starTimestep.star_mdot = json.star_mdot[i];
   timestepsArr.push(starTimestep);
 }
-console.log(timestepsArr.length);
